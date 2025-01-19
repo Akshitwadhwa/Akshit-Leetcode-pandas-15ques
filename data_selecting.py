e9df08d2da7b8c54184196a8bdcd643e406fa37b
akshit_wadhwa <akshitwadhwa06@gmail.com>
2025-01-19 17:14:07 +0530

    Update data_selecting.py

--- data_selecting.py
+++ data_selecting.py
@@ -1,1 +1,18 @@
+Q1----2880 
 
+Here we have been given a table in which we have 3 columns called student_id, name, and age
+-- The question is we have to find the solution for selecting the columns with student name = 101 and the respective name and age column.
+So here we will use the return statement so that we can get the exact column which we need.
+So the solution code will be
+
+import pandas as pd
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.loc[students["student_id"] == 101 , ["name" , "age"]]
+
+
+Q2----2881
+
+In This question, we have to create a new column that has twice the values of the salary columns.
+Here we have the name and salary
+    
