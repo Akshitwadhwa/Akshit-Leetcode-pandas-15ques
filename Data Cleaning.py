5217c49bf6b2ab26b785e28537a18eaf3a44f656
akshit_wadhwa <akshitwadhwa06@gmail.com>
2025-01-21 00:18:42 +0530

    Update Data Cleaning.py

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -1,2 +1,8 @@
 Q1--- 2882
-Here we have a question where we have
+Here we have a question where we have to delete a given column in a certain table so we will use the return command and drop command 
+we have to remove the duplicate emails in the given data frame so the code will be
+
+import pandas as pd
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    return customers.drop_duplicates('email')
