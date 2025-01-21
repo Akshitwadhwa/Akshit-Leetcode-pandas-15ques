5fa059680b0c0386510ff6f1204e49dc44a12f59
akshit_wadhwa <akshitwadhwa06@gmail.com>
2025-01-21 23:58:42 +0530

    Update Data Cleaning.py

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -6,3 +6,20 @@ import pandas as pd
 
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
     return customers.drop_duplicates('email')
+
+# Here we can see that the email has been dropped after this
+
+Q2--- 2883
+( This is a drop missing data question )
+In the table there are 3 tables called student_id , name , age
+We have to find the null value in the name column and remove that value.
+So we will use the dropna command.
+
+import pandas as pd
+
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+   return students.dropna(subset="name")
+
+---------
+Q3--- 2884
+( In this we have to play with the data where we have to make a new salary columns with making 
